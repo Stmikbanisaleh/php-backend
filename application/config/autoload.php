583a59f8fd523b5f2690bf8f6559746e3c691b9e
imamsatrianta <imamsatrianta@gmail.com>
2019-11-13 13:23:01 +0700

    update database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('Lapan_api_library','database', 'email', 'session', 'form_validation');
+$autoload['libraries'] = array('Lapan_api_library', 'email', 'session', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
